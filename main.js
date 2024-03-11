5e8dda156572ba6016d95020b750c498675ad05e
Estevao <estevaomoraes2009@hotmail.com>
2024-03-11 16:10:02 -0300

    correçao do arquivo main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const novaAtividade = $('#form-nome').val();
-const novoItem = $(`<li style="display: none"></li>`);
+
 
 
 
 $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
-        const novoItem = $(`<li style="display: none"></li>`);
+
+    const novaAtividade = $('#form-nome').val();
+    const novoItem = $(`<li style="display: none"></li>`);
+    
         $('.container-2').slideDown();
         $(`<input type="checkbox" id="Atividade" name="Atividade" unchecked />
-        <label for="Atividade">${novaAtividade.value}</label>`).appendTo(novoItem);
+        <label for="Atividade">${novaAtividade}</label>`).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn();
     });
